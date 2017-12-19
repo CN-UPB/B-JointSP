05ec53f5824c195db711ff9149c93bfe40b3425a
Stefan Schneider <st.b.s@web.de>
2017-12-19 17:02:58 +0100

    created branch, moved code into src, created setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='bjointsp',
+    version='1.0',
+    license='Apache 2.0',
+    description='B-JointSP provides algorithms for joint placement and scaling of bidirectional network services',
+    url='https://github.com/CN-UPB/B-JointSP',
+    author_email='stefan.schneider@upb.de',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=[
+        "gurobipy",
+    ],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'bjointsp=main:main',
+        ],
+    },
+)
