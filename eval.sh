27ee711dca8abcb70457058af8fe503fe413600e
Stefan Schneider <stefan.schneider@upb.de>
2019-11-22 17:40:31 +0100

    added more sources and eval script

---
+++ eval.sh
@@ -0,0 +1,5 @@
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web.yaml -s parameters/sources/web_eval/s0.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web.yaml -s parameters/sources/web_eval/s01.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web.yaml -s parameters/sources/web_eval/s012.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web.yaml -s parameters/sources/web_eval/s0123.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web.yaml -s parameters/sources/web_eval/s01234.yaml
