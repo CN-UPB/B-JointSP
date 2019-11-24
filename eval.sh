9c7d40cacd7b9a5da057a91f60a3e023a4925dae
Stefan Schneider <stefan.schneider@upb.de>
2019-11-24 14:10:44 +0100

    fixed script and inputs for eval

--- eval.sh
+++ eval.sh
@@ -6,10 +6,13 @@
 #bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web_fixed.yaml -s parameters/sources/synth_data/single_source_dr1/s01234.yaml
 
 # multi flow, single source
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web_fixed.yaml -s parameters/sources/web3_small/single_source_dr250/s0-f1.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web_fixed.yaml -s parameters/sources/web3_small/single_source_dr250/s0-f2.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web_fixed.yaml -s parameters/sources/web3_small/single_source_dr250/s0-f3.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web_fixed.yaml -s parameters/sources/web3_small/single_source_dr250/s0-f4.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web_fixed.yaml -s parameters/sources/web3_small/single_source_dr250/s0-f5.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web_fixed.yaml -s parameters/sources/web3_small/single_source_dr250/s0-f6.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web_fixed.yaml -s parameters/sources/web3_small/single_source_dr250/s0-f7.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web_fixed.yaml -s parameters/sources/web_data/single_source_dr250/s0-f1.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web_fixed.yaml -s parameters/sources/web_data/single_source_dr250/s0-f2.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web_fixed.yaml -s parameters/sources/web_data/single_source_dr250/s0-f3.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web_fixed.yaml -s parameters/sources/web_data/single_source_dr250/s0-f4.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web_fixed.yaml -s parameters/sources/web_data/single_source_dr250/s0-f5.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web_fixed.yaml -s parameters/sources/web_data/single_source_dr250/s0-f6.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web_fixed.yaml -s parameters/sources/web_data/single_source_dr250/s0-f7.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web_fixed.yaml -s parameters/sources/web_data/single_source_dr250/s0-f8.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web_fixed.yaml -s parameters/sources/web_data/single_source_dr250/s0-f9.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web_fixed.yaml -s parameters/sources/web_data/single_source_dr250/s0-f10.yaml
