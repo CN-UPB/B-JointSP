6c6a4199e6249c261f37c35f691f210a61da7e55
Stefan Schneider <stefan.schneider@upb.de>
2019-11-26 17:24:14 +0100

    updated inputs

--- eval.sh
+++ eval.sh
@@ -1,44 +1,68 @@
 # multi source, single flow
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_fixed.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f1.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_fixed.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f2.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_fixed.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f3.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_fixed.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f4.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_fixed.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f5.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_fixed.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f6.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_fixed.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f7.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_fixed.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f1.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_fixed.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f2.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_fixed.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f3.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_fixed.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f4.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_fixed.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f5.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_fixed.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f6.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_fixed.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f7.yaml
+#
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_linear.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f1.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_linear.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f2.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_linear.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f3.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_linear.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f4.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_linear.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f5.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_linear.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f6.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_linear.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f7.yaml
+#
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_true.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f1.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_true.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f2.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_true.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f3.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_true.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f4.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_true.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f5.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_true.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f6.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_true.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f7.yaml
+#
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_svr.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f1.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_svr.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f2.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_svr.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f3.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_svr.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f4.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_svr.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f5.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_svr.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f6.yaml
+#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_svr.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f7.yaml
 
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_linear.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f1.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_linear.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f2.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_linear.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f3.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_linear.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f4.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_linear.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f5.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_linear.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f6.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_linear.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f7.yaml
 
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_true.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f1.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_true.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f2.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_true.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f3.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_true.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f4.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_true.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f5.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_true.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f6.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_true.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f7.yaml
-
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_svr.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f1.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_svr.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f2.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_svr.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f3.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_svr.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f4.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_svr.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f5.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_svr.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f6.yaml
-bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/synth2_ml_svr.yaml -s parameters/sources/synth_data/single_source_dr1/s0-f7.yaml
 
 # multi flow, single source
-#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml_boosting.yaml -s parameters/sources/web_data/single_source_dr250/s0-f1.yaml
-#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml_boosting.yaml -s parameters/sources/web_data/single_source_dr250/s0-f2.yaml
-#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml_boosting.yaml -s parameters/sources/web_data/single_source_dr250/s0-f3.yaml
-#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml_boosting.yaml -s parameters/sources/web_data/single_source_dr250/s0-f4.yaml
-#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml_boosting.yaml -s parameters/sources/web_data/single_source_dr250/s0-f5.yaml
-#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml_boosting.yaml -s parameters/sources/web_data/single_source_dr250/s0-f6.yaml
-#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml_boosting.yaml -s parameters/sources/web_data/single_source_dr250/s0-f7.yaml
-#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml_boosting.yaml -s parameters/sources/web_data/single_source_dr250/s0-f8.yaml
-#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml_boosting.yaml -s parameters/sources/web_data/single_source_dr250/s0-f9.yaml
-#bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml_boosting.yaml -s parameters/sources/web_data/single_source_dr250/s0-f10.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_fixed.yaml -s parameters/sources/web_data/three_source_dr250/s0-f1.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_fixed.yaml -s parameters/sources/web_data/three_source_dr250/s0-f2.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_fixed.yaml -s parameters/sources/web_data/three_source_dr250/s0-f3.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_fixed.yaml -s parameters/sources/web_data/three_source_dr250/s0-f4.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_fixed.yaml -s parameters/sources/web_data/three_source_dr250/s0-f5.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_fixed.yaml -s parameters/sources/web_data/three_source_dr250/s0-f6.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_fixed.yaml -s parameters/sources/web_data/three_source_dr250/s0-f7.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_fixed.yaml -s parameters/sources/web_data/three_source_dr250/s0-f8.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_fixed.yaml -s parameters/sources/web_data/three_source_dr250/s0-f9.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_fixed.yaml -s parameters/sources/web_data/three_source_dr250/s0-f10.yaml
+
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_linear.yaml -s parameters/sources/web_data/three_source_dr250/s0-f1.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_linear.yaml -s parameters/sources/web_data/three_source_dr250/s0-f2.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_linear.yaml -s parameters/sources/web_data/three_source_dr250/s0-f3.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_linear.yaml -s parameters/sources/web_data/three_source_dr250/s0-f4.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_linear.yaml -s parameters/sources/web_data/three_source_dr250/s0-f5.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_linear.yaml -s parameters/sources/web_data/three_source_dr250/s0-f6.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_linear.yaml -s parameters/sources/web_data/three_source_dr250/s0-f7.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_linear.yaml -s parameters/sources/web_data/three_source_dr250/s0-f8.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_linear.yaml -s parameters/sources/web_data/three_source_dr250/s0-f9.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_linear.yaml -s parameters/sources/web_data/three_source_dr250/s0-f10.yaml
+
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml.yaml -s parameters/sources/web_data/three_source_dr250/s0-f1.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml.yaml -s parameters/sources/web_data/three_source_dr250/s0-f2.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml.yaml -s parameters/sources/web_data/three_source_dr250/s0-f3.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml.yaml -s parameters/sources/web_data/three_source_dr250/s0-f4.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml.yaml -s parameters/sources/web_data/three_source_dr250/s0-f5.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml.yaml -s parameters/sources/web_data/three_source_dr250/s0-f6.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml.yaml -s parameters/sources/web_data/three_source_dr250/s0-f7.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml.yaml -s parameters/sources/web_data/three_source_dr250/s0-f8.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml.yaml -s parameters/sources/web_data/three_source_dr250/s0-f9.yaml
+bjointsp -n parameters/networks/Abilene.graphml -t parameters/templates/web2_ml.yaml -s parameters/sources/web_data/three_source_dr250/s0-f10.yaml
